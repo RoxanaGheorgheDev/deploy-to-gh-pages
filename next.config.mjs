96b8e0c59b9f6c5485a98d84fb43dcc48eaaa054
Roxana Gheorghe <roxana.anca.gheorghe@gmail.com>
2024-11-29 14:54:38 +0200

    Modified config for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /*Creating the deploy folder*/
+  output: "export",
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
+  // basePath allows you to set a path prefix for the application when deploying
+  basePath: "/deploy-to-gh-pages",
+};
 
 export default nextConfig;
